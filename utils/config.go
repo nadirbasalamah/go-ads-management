3f6bbf8bd1e9717a55582bb275af7c477b66126b
nadirbasalamah <nadir008basalamah@gmail.com>
2024-11-19 07:57:29 +0700

    feat(docker): add docker support

--- utils/config.go
+++ utils/config.go
@@ -2,17 +2,24 @@ package utils
 
 import (
 	"log"
+	"os"
 
 	"github.com/spf13/viper"
 )
 
 func GetConfig(key string) string {
-	viper.AddConfigPath(".")
-	viper.SetConfigFile(".env")
+	isDevelop := os.Getenv("APP_MODE") != "production"
 
-	if err := viper.ReadInConfig(); err != nil {
-		log.Fatalf("error when reading configuration file: %s\n", err)
+	if isDevelop {
+		viper.AddConfigPath(".")
+		viper.SetConfigFile(".env")
+
+		if err := viper.ReadInConfig(); err != nil {
+			log.Fatalf("error when reading configuration file: %s\n", err)
+		}
+
+		return viper.GetString(key)
 	}
 
-	return viper.GetString(key)
+	return os.Getenv(key)
 }
